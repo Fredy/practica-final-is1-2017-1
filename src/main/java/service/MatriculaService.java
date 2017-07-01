cedcd67c0aaca7e4cf1daaa1914d685c1998b173
Fredy Álvarez <fre.alvarezb@gmail.com>
2017-07-01 18:01:07 -0500

    -Matricula service

--- src/main/java/service/MatriculaService.java
+++ src/main/java/service/MatriculaService.java
@@ -1,7 +1,21 @@
 package service;
 
+import domain.Matricula;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.MatriculaRepository;
+
+import java.util.Collection;
+
 /**
  * Created by fredy on 01/07/17.
  */
+@Service
 public class MatriculaService {
+    @Autowired
+    MatriculaRepository matriculaRepository;
+
+    public Collection<Object> srchNombre(Long id,String semestre) {
+        return matriculaRepository.srchCursoSemestre(id, semestre);
+    }
 }
